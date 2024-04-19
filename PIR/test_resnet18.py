f7e84d362034b6756447994a4ff3bbd946b969c0
Yi Liu <2681514899@qq.com>
2024-04-19 17:05:02 +0800

    resnet18 test file

---
+++ PIR/test_resnet18.py
@@ -0,0 +1,49 @@
+from utils import TestBase
+
+import paddle
+from paddle.vision.models import resnet18
+import paddle.profiler as profiler
+
+
+class TestResNet18(TestBase):
+     def __init__(self):
+          super().__init__()
+          self.prepare_net()
+          self.prepare_data()
+     def prepare_net(self):
+          self.net = resnet18(pretrained=True)
+     def prepare_data(self):
+          self.batch_size = 32
+          self.input_shape = [self.batch_size, 3, 224, 224]
+          self.input = paddle.rand(self.input_shape)
+     def profile(self, use_cinn):
+          print("--[profile] profile %s" % ("cinn" if use_cinn else "nocinn"))
+          net = self.to_eval(use_cinn)
+          # 创建性能分析器 Profiler 对象，并配置参数。
+          # 分析第 [15, 17）次迭代区间的性能数据。
+          prof = profiler.Profiler(targets=[profiler.ProfilerTarget.CPU, profiler.ProfilerTarget.GPU],
+                              scheduler = (15, 17),
+                              on_trace_ready = profiler.export_chrome_tracing('./profiler_data/resnet18/hygon', 'bs%s_%s' % (self.batch_size, 'withcinn' if use_cinn else "nocinn")),
+                              timer_only = False)
+          prof.start()
+          for iteration in range(20):
+               # 为作用域代码加"resent18 eval"的标签
+               with profiler.RecordEvent(name="resent18 eval"):
+                    output = net(self.input)
+                    paddle.device.synchronize()
+               prof.step()
+          prof.stop()
+          prof.summary(sorted_by=profiler.SortedKeys.GPUTotal,
+                    op_detail=True,
+                    thread_sep=False,
+                    time_unit='ms')
+
+if __name__ == '__main__':
+     model = TestResNet18()
+     model.check_cinn_ouput()
+     model.benchmark(use_cinn=False)
+     model.check_cinn_ouput()
+     model.benchmark(use_cinn=True)
+     model.profile(use_cinn=True)
+
+# python3 test_resnet18.py &>> ./profiler_data/resnet18/hygon/bs32.log
---
+++ PIR/utils.py
@@ -0,0 +1,74 @@
+import paddle
+import paddle.profiler as profiler
+import os
+import time
+import numpy as np
+
+def set_flags(use_cinn):
+    if use_cinn:
+        os.environ['FLAGS_cinn_new_group_scheduler'] = '1'
+        os.environ['FLAGS_group_schedule_tiling_first'] = '1'
+        os.environ['FLAGS_prim_all'] = 'true'
+        os.environ['FLAGS_prim_enable_dynamic'] = 'true'  
+        os.environ['FLAGS_print_ir'] = '1'
+        os.environ['FLAGS_enable_pir_api'] = '1'
+        os.environ['FLAGS_use_cinn'] = '1'
+        os.environ['FLAGS_cinn_bucket_compile'] = '1'
+        os.environ['FLAGS_cinn_new_cluster_op_method'] = '1'
+    else:
+        os.environ['FLAGS_use_cinn'] = '0'
+
+def apply_to_static(net, use_cinn, input_spec=None):
+    build_strategy = paddle.static.BuildStrategy()
+    build_strategy.build_cinn_pass = use_cinn
+    return paddle.jit.to_static(
+        net,
+        input_spec=input_spec,
+        build_strategy=build_strategy,
+        full_graph=True,
+    )
+
+def benchmark(net, input, repeat=50, warmup=10):
+    # warm up
+    for i in range(warmup):
+        net(input)
+    # time
+    t = []
+    for i in range(repeat):
+        t1 = time.time()
+        output = net(input)
+        t2 = time.time()
+        t.append((t2 - t1)*1000)
+    print("--[benchmark] Run for %d times, the average latency is:%f ms" % (repeat, np.mean(t)))
+
+class TestBase:
+    def __init__(self):
+        self.net = None
+        self.input = None
+        self.cinn_net = None
+    def to_eval(self, use_cinn):
+        set_flags(use_cinn)
+        if use_cinn:
+            if not self.cinn_net:
+                self.cinn_net = apply_to_static(self.net, use_cinn)
+                self.prepare_net()
+            net = self.cinn_net
+        else:
+            net = self.net
+        net.eval()
+        return net
+    def eval(self, use_cinn):
+        net = self.to_eval(use_cinn)
+        out = net(self.input)
+        return out
+    def check_cinn_ouput(self):
+        cinn_out = self.eval(use_cinn=True)
+        dy_out = self.eval(use_cinn=False)
+        np.testing.assert_allclose(
+            cinn_out.numpy(), dy_out.numpy(), atol=1e-5, rtol=1e-5
+        )
+        print("--[check_cinn_ouput] cinn result right.")
+    def benchmark(self, use_cinn):
+        print("--[benchmark] benchmark %s" % ("cinn" if use_cinn else "nocinn"))
+        net = self.to_eval(use_cinn)
+        benchmark(net, self.input)
